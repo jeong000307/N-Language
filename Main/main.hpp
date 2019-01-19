e6f6fb5629808f554660cac867b37eb57c58802a
Gyu-Yong Jeong <jeonggyuyong@Gyu-Yongui-MacBookPro.local>
2019-01-19 20:05:15 +0900

    Hi

---
+++ Main/main.hpp
@@ -0,0 +1,12 @@
+#ifndef __MAIN_HPP__
+#define __MAIN_HPP__
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include "CodeGenerator.hpp"
+
+int LexicalAnalysis(std::string &inputString);
+
+#endif 
